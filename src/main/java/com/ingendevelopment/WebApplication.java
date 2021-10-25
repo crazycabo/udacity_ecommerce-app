2af911078f748eeb2f0c45f26a7ee7c5001845ed
Brian Smith <crazycabo350@gmail.com>
2021-10-25 10:06:59 -0400

    Add security dependencies and disable default security

--- src/main/java/com/ingendevelopment/WebApplication.java
+++ src/main/java/com/ingendevelopment/WebApplication.java
@@ -3,15 +3,16 @@ package com.ingendevelopment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaRepositories("com.ingendevelopment.model.persistence.repositories")
 @EntityScan("com.ingendevelopment.model.persistence")
-@SpringBootApplication
-public class SareetaApplication {
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+public class WebApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SareetaApplication.class, args);
+		SpringApplication.run(WebApplication.class, args);
 	}
 
 }
