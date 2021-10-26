dac52223d89a8b5e5b8455b62fea602b474e0e57
Brian Smith <crazycabo350@gmail.com>
2021-10-26 12:05:57 -0400

    Remove redundant equals and hashCode methods

--- src/main/java/com/ingendevelopment/model/persistence/Item.java
+++ src/main/java/com/ingendevelopment/model/persistence/Item.java
@@ -3,6 +3,7 @@ package com.ingendevelopment.model.persistence;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -22,6 +23,7 @@ import java.math.BigDecimal;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode
 public class Item {
 
 	@Id
@@ -40,29 +42,4 @@ public class Item {
 	@Column(nullable = false)
 	@JsonProperty
 	private String description;
-	
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Item other = (Item) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
 }
